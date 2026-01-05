5f639dac65f0e1c8dc5e3ee062e93f7e4a92375d
sapat <rkamble@sapat.com>
2026-01-05 14:59:10 +0530

    alignmnet issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3829531167"
+    serviceWorkerVersion: "4047125967"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,14 +33,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "af9d6077f700e60564cc8f4ced3f155f
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "e1d8437bf014967c6e0fd97959cda74c",
+"flutter_bootstrap.js": "2f26f91eb097c61f886230bafe82c063",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "641c2eccb1451b73798265a1dc26f48a",
 "/": "641c2eccb1451b73798265a1dc26f48a",
-"main.dart.js": "de6fee4c7dfb5903b7e25cef52824114",
+"main.dart.js": "a46d68bf7eacc1d8d66a8398adb3a9b6",
 "manifest.json": "79c4ca871a7c6e05d034457c0d5d839c",
 "version.json": "6b2cbf0bc9be9a4c6e4431bd3c9a33b6"};
 // The application shell files that are downloaded before a service worker can
